63683f65a82b69ec34aa16361e27d6856d20f29c
codethoisinhvien <nguyentruonggiang28081999@gmail.com>
2019-07-06 16:34:31 +0700

    ss

--- Model/config.py
+++ Model/config.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_mongoalchemy import BaseQuery
 from flask_mongoalchemy import MongoAlchemy
 app = Flask(__name__)
-app.config['MONGOALCHEMY_DATABASE'] = 'dicecoin'
+app.config['MONGOALCHEMY_DATABASE'] = 'heroku_cjxxw05p'
+app.config['MONGOALCHEMY_CONNECTION_STRING']="mongodb://root:Phongthien1308@ds247637.mlab.com:47637/heroku_cjxxw05p"
 db = MongoAlchemy(app)
 
 class Seed(db.Document):
